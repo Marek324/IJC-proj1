bf78cc397fe1bf0a46469fb617eb902891adec78
Marek324 <hrmajo1@gmail.com>
2024-03-20 18:52:45 +0100

    Implemented FST in no-comment.c

--- no-comment.c
+++ no-comment.c
@@ -17,37 +17,85 @@ int main(int argc, char *argv[]){
     while ((c=getchar()) != EOF) {
         switch(stav) {
             
-            case 0: if(c=='/') stav=1;
-                else if (c=='"') { stav=4; putchar(c); }
-                else putchar(c);
+            case 0: 
+                if(c=='/') {
+                    stav=1;
+                }
+                else if (c=='"') { 
+                    stav=6; 
+                    putchar(c); 
+                }
+                else if(c=='\''){
+                    stav=7;
+                    putchar(c);
+                }
+                else {
+                    putchar(c);
+                }
                 break;
             
             case 1: 
-                if(c=='*') stav=2;
-                else if(c=='/') putchar(c);
-                else { stav=0; putchar('/'); putchar(c); }
+                if(c=='/') {
+                    stav=2;
+                }
+                else if (c=='*'){
+                    stav = 4;
+                }
+                else {
+                    stav = 0;
+                    putchar('/');
+                    putchar(c);
+                }
                 break;
             
             case 2: 
-                if(c=='*') stav=3;
+                if(c=='\\'){
+                    stav = 3;
+                } else if(c == '\n'){
+                    stav = 0;
+                    putchar('\n');
+                }
                 break;
             
             case 3: 
-                if(c=='/') { stav=0; putchar(' '); }
-                else if(c!='*') stav=2;
+                if(c != '\\') {
+                    stav = 2;
+                }
                 break;
             
             case 4: 
-                if(c=='\\') stav=5;
-                else if(c=='"') stav=0;
-                putchar(c);
+                if(c == '*'){
+                    stav = 5;
+                    break;
+                } 
                 break;
             
-            case 5: stav=4;
-                putchar(c);
+            case 5: 
+                if(c != '*' && c != '/'){
+                    stav = 4;
+                } else if (c == '/'){
+                    stav = 0;
+                    putchar(' ');
+                }
                 break;
 
-            //TODO: pridat stavy
+            case 6: 
+                if(c == '"'){
+                    stav = 0;
+                    putchar('"');
+                } else {
+                    putchar(c);
+                }
+                break;
+
+            case 7: 
+                if(c == '\''){
+                    stav = 0;
+                    putchar('\'');
+                } else {
+                    putchar(c);
+                }
+                break;
         
         } 
     } 
